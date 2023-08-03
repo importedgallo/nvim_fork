5350b84fe710f678e9ca401cb2bee875f4b62c77
importedgallo <andre.mongard@gmail.com>
2023-08-03 14:19:28 +1000

    disabled window navigation mappings to enable tmux-navigator mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,12 +1,21 @@
-local M = {
+local M = {}
 
-}
 -- M.general = {
 --   n = {
 --     ["+"] = {"<C-a", "Increment"},
 --     ["-"] = {"<C-x", "Decrement"},
 --   }
 -- }
+
+--diasble these to enable tmux-navigatior mappings
+M.disabled = {
+  n = {
+    ["<C-h>"] = { "<C-w>h", "Window left" },
+    ["<C-l>"] = { "<C-w>l", "Window right" },
+    ["<C-j>"] = { "<C-w>j", "Window down" },
+    ["<C-k>"] = { "<C-w>k", "Window up" },
+  }
+}
 
 M.dap = {
   plugin = true,
